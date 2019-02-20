ebcae5781c9d2ce625656bc122d37b944b4e6cf6
Zsuzsanna Erdos <ezsana@gmail.com>
2019-02-20 17:16:35 +0100

    Milestones 1-2-3 ready

--- calendar.py
+++ calendar.py
@@ -8,14 +8,17 @@ def choose():
     inputs = ui.get_inputs(['Please choose between the options: '])
     user_choice = inputs[0]
     if user_choice == 's':
-        ui.simple_print('Your choice: s')
+        ui.simple_print('Schedule a new meeting.')
         storage.schedule()
     elif user_choice == 'c':
-        ui.simple_print('Your choice: c')
         storage.cancel()
     elif user_choice == 'm':
         ui.simple_print('Your schedule for the day: ')
         print(storage.read_from_file())
+    elif user_choice == 'e':
+        ui.simple_print('Your schedule for the day: ')
+        print(storage.read_from_file())
+        storage.edit_meeting()
     elif user_choice == 'q':
         ui.simple_print('Your choice: q')
         sys.exit()
@@ -27,6 +30,7 @@ def handle_menu():
     options = [ 's: Schedule a new meeting',
                 'c: Cancel an existing meeting',
                 'm: Your meetings',
+                'e: Edit a meeting',
                 'q: Quit']
     ui.print_menu('Menu', options)
 
--- storage.py
+++ storage.py
@@ -4,33 +4,91 @@ import calendar
 
 
 def schedule():
-    ui.simple_print('Schedule a new meeting.')
     labels = ['Enter meeting title: ', 'Enter start time: ', 'Enter duration in hours (1 or 2): ']
     add_meeting = []
     for l in range(len(labels)):
         if l == 0:
             answer = ui.simple_input(labels[l])
         else:
-            answer = str(ui.integer_input(labels[l]))
+            if l == 1:
+                while True:
+                    answer = ui.integer_input(labels[l])
+                    if answer not in range(8, 18):
+                        ui.simple_print('Between 8-17 hours please.')
+                        continue
+                    else:
+                        break
+            if l == 2:
+                while True:
+                    answer = ui.integer_input(labels[l])
+                    if answer not in range(1, 3):
+                        ui.simple_print('1 or 2 hours meeting?')
+                        continue
+                    else:
+                        break
+            answer = str(answer)
         add_meeting.append(answer)
     add_meeting[-1] = add_meeting[-1] + ' hour(s)'
     meeting = ' / '.join(add_meeting)
     with open('/home/zsana/python/Calendar/store_datas.csv', "a") as file:
         file.write(meeting + '\n')
     ui.simple_print('Meeting added.')
-    '''
+
 
 def cancel():
     ui.simple_print('Cancel an existing meeting.')
-
-    Enter the start time: 13
-    ERROR: There is no meeting starting at that time!
-    Enter the start time: 12
-    Meeting cancelled.
-    '''
+    lines = read_from_file().split('\n')
+    new_lines = []
+    for i in lines[:-1]:
+        x = i.split(' / ')
+        new_lines.append(x)
+    meeting_times = []
+    for n in new_lines:
+        meeting_times.append(n[1])
+    while True:
+        meeting_to_cancel = ui.integer_input('Enter the start time: ')
+        if str(meeting_to_cancel) not in meeting_times:
+            ui.simple_print('There\'s no meeting at that time.')
+            continue
+        else:
+            for n in new_lines:
+                if str(meeting_to_cancel) == n[1]:
+                    new_lines.remove(n)
+            break
+    write_to_file(new_lines)
+    ui.simple_print('Meeting cancelled.')
 
 
 def read_from_file():
     with open('/home/zsana/python/Calendar/store_datas.csv', 'r') as f:
         lines = f.read()
     return lines
+
+
+def write_to_file(lines):
+    with open('/home/zsana/python/Calendar/store_datas.csv', 'w') as f:
+        for l in lines:
+            row = ' / '.join(l)
+            f.write(row + '\n')
+
+
+def edit_meeting():
+    lines = read_from_file().split('\n')
+    new_lines = []
+    for i in lines[:-1]:
+        x = i.split(' / ')
+        new_lines.append(x)
+    titles = []
+    for n in new_lines:
+        titles.append(n[0])
+    while True:
+        edit_meeting = ui.simple_input('Which meeting would you like to edit? ')
+        if edit_meeting not in titles:
+            ui.simple_print('There is no such meeting.')
+            continue
+        else:
+            for meeting in new_lines:
+                if edit_meeting == meeting[0]:
+                    new_lines.remove(meeting)
+            break
+    schedule()
